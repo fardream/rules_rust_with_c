5e2e9b73d9139495e0e43dcbf37d5341af782d53
Chao Xu <fardream@users.noreply.github.com>
2024-01-28 18:56:08 -0500

    tokio

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,23 @@
+use tokio::fs::File;
 extern "C" {
     fn simple_sin(x: f64) -> f64;
 }
 
 pub fn rs_simple_sin(x: f64) -> f64 {
     unsafe { simple_sin(x) }
+}
+
+pub async fn good_fn() -> String {
+    let f = File::open("foo").await;
+
+    match f {
+        Ok(_) => {
+            format!("success")
+        }
+        Err(_) => {
+            format!("failed")
+        }
+    }
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use rules_rust_with_c::rs_simple_sin;
+use rules_rust_with_c::{good_fn, rs_simple_sin};
 
 #[tokio::main]
 async fn main() {
+    println!("{}", good_fn().await);
     println!("{}", rs_simple_sin(9.0));
 }
